9a68db141432ecac6c55289d63f5d23c7539db79
Mietunteol <mietunteol@protonmail.com>
2019-08-21 08:35:50 +0000

    Add codewars.com::python[ 8:8 | 7:1 | 6:0 | 5:0 | 4:0 | 3:0 | 2:0 | 1:0 ]

---
+++ codewars.com/python/kyu_7/remove_duplicate_words.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/remove-duplicate-words
+
+
+def remove_duplicate_words(s):
+    result = []
+    for word in s.split(' '):
+        if word not in result:
+            result.append(word)
+    return ' '.join(result)
